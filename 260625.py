1a687f2e5b2fdbbff8dad5f9fb8337c32ce1ebda
jakehoohoo-blip <jakehoohoo@gmail.com>
2026-06-25 17:47:34 +0900

    Add files via upload
    딕셔너리

--- 260625.py
+++ 260625.py
@@ -20,6 +20,27 @@
 # del user["city"]
 # print(user)
 
-scores = {"국어":80,"수학":100,"영어":95}
-target = input("점수를 검색할 과목 입력 : ")
-print(scores[target])
+# scores = {"국어":80,"수학":100,"영어":95}
+# target = input("점수를 검색할 과목 입력 : ")
+# print(scores[target])
+
+# while True:
+#     random_box = {"1":"핸드폰","2":"바퀴벌레🪳","3":"농구공","4":"종료"}
+#     print("===🎁 운명의 보물상자 게임 ===")
+#     print("앞에 3개의 상자가 있습니다. 몇 번 상자를 여시겠습니까?")
+#     choice = input("선택(1,2,3,4(종료)) : ")
+#     if choice != "1" or choice != "2" or choice != "3" or choice != "4":
+#         print("선택지에 없는 수입니다. 다시 골라주세요")
+#         continue
+#     elif choice == "1":
+#         print("보상은",random_box[1],"입니다.")
+#         continue
+#     elif choice == "2":
+#         print("보상은",random_box[2],"입니다.")
+#         continue
+#     elif choice == "3":
+#         print("보상은",random_box[3],"입니다.")
+#         continue
+#     elif choice == "4":
+#         print("끝입니다.")
+#         break
