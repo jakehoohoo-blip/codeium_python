1ff08dbfa9c58ed0e4eb39f2c9780623edd740a9
jakehoohoo-blip <jakehoohoo@gmail.com>
2026-06-25 17:45:54 +0900

    딕셔너리

---
+++ 260625.py
@@ -0,0 +1,25 @@
+# student = {"name":"손윤후","age":"14","city":"서울"}
+# print(student["name"])
+# print(student["age"])
+# print(student["city"])
+
+# user = {}
+# user["name"] = "윤후"
+# user["age"] = "14"
+# print(user)
+
+# user = {"name":"윤후","age":"14"}
+# user["age"] = 20
+# print(user)
+
+# monster = {"name":"슬라임","hp":50}
+# monster["hp"]=30
+# print(monster)
+
+# user = {"name":"철수","age":18,"city":"서울"}
+# del user["city"]
+# print(user)
+
+scores = {"국어":80,"수학":100,"영어":95}
+target = input("점수를 검색할 과목 입력 : ")
+print(scores[target])
